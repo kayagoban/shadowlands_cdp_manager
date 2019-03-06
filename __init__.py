35350a4e4e85d4e6ba5338f85c89aa49c5c3e6ff
C. Thomas <cthomas@soykaf.digital>
2019-03-06 14:12:46 -0600

    First stab at user interface - status panel

--- __init__.py
+++ __init__.py
@@ -34,119 +34,45 @@ from cdp_manager.contracts.oasis_proxy import OasisProxy
 from cdp_manager.contracts.proxy_registry import ProxyRegistry
 from cdp_manager.contracts.dai import Dai
 
+class CDPStatusFrame(SLFrame):
+
+    def drop(self):
+        debug(); pdb.set_trace()
+
+    def initialize(self):
+        self.add_divider()
+        self.add_label_pair("Liquidation Price: ", str(round(self.dapp.liquidation_price(self.dapp.cup_id), 4)) + " USD", add_divider=False)
+        self.add_label_pair("Current Price: (ETH/USD)", str(round(self.dapp.ether_price(), 4)) + " USD", add_divider=False)
+        self.add_label_pair("Liquidation penalty: ", str(round(self.dapp.liquidation_penalty(), 4)) + " %", add_divider=False)
+        self.add_divider(draw_line=True)
+        self.add_label_pair("Collateralization Ratio: ", str(round(self.dapp.collateralization_ratio(self.dapp.cup_id), 4)) + " %", add_divider=False)
+        self.add_label_pair("Minimum Ratio:", str(round(self.dapp.liquidation_ratio(), 4) * 100) + " %", add_divider=False)
+        self.add_label_pair("Stability Fee:", str(round(self.dapp.stability_fee(), 4)) + " %", add_divider=False)
+        self.add_divider(draw_line=True)
+        self.add_label("ETH Collateral")
+        self.add_label_pair("Deposited:", str(round(self.dapp.collateral_eth_value(self.dapp.cup_id) / self.dapp.WAD, 4)) +  " ETH", add_divider=False)
+        self.add_label_pair("", str(round(self.dapp.collateral_peth_value(self.dapp.cup_id) / self.dapp.WAD, 4)) +  " PETH", add_divider=False)
+        self.add_label_pair("", str(round( Decimal(self.dapp.pip.eth_price() / self.dapp.WAD) * self.dapp.collateral_eth_value(self.dapp.cup_id) / self.dapp.WAD, 4)) +  " USD", add_divider=False)
+        self.add_button(self.close, "DEPOSIT", layout_distribution = [1, 1, 1, 1], layout_index=0)
+        self.add_divider(draw_line=False)
+        self.add_label_pair("Max avail to withdraw:", str(round(self.dapp.eth_available_to_withdraw(self.dapp.cup_id), 3)) + " ETH", add_divider=False)
+        self.add_label_pair("", str(round(self.dapp.peth_available_to_withdraw(self.dapp.cup_id), 3)) + " PETH", add_divider=False)
+        self.add_label_pair("", str(round(self.dapp.eth_available_to_withdraw(self.dapp.cup_id) * Decimal(self.dapp.pip.eth_price()) / self.dapp.WAD)) + " USD", add_divider=False)
+        self.add_button(self.close, "WITHDRAW", layout_distribution = [1, 1, 1, 1], layout_index=0, add_divider=False)
+        self.add_divider()
+        self.add_divider(draw_line=False)
+        self.add_ok_cancel_buttons(self.drop)
+
 
 class Dapp(SLDapp):
 
+    cup_id = 15341
     RAY = Decimal(10 ** 27)
     WAD = Decimal(10 ** 18)
 
-    def peth_price(self):
-        per = Decimal(self.tub.per())
-        return per / self.RAY
- 
-    def collateral_eth_value(self, cup_id):
-       return self.peth_price() * self.collateral_peth_value(cup_id)
-        
-    def liquidation_price(self, cup_id):
-        debt_value = Decimal(self.tub.tab(cup_id)) # not quite, but..
-        mat = Decimal(self.tub.mat())
-        liquidation_ratio = mat / self.RAY 
-        return debt_value * liquidation_ratio / self.collateral_eth_value(cup_id)
-
-    def collateral_peth_value(self, cup_id):
-        return Decimal(self.tub.ink(cup_id))
-
-    # abstract collateral price
-    def tag(self):
-        return Decimal(self.tub.tag()) / self.RAY
-
-    def liquidation_penalty(self):
-        axe = self.tub.axe()
-        penalty = ((axe / self.RAY) - 1) * 100
-        return round(Decimal(penalty), 2)
-
-    def debt_value(self, cup_id):
-        return Decimal(self.tub.tab(cup_id))
-
-    def global_dai_available(self):
-        return Decimal(self.tub.rum()) 
-
-    def target_price(self):
-        return Decimal(self.vox.par())
-
-    def collateralization_ratio(self, cup_id):
-        tag = Decimal(self.tub.tag())
-        #usd_debt = tag * self.target_price()
-        #per = Decimal(self.tub.per())
-        #peth_price = per / self.RAY
-        coll_ratio = self.collateral_peth_value(cup_id) * (tag / self.RAY) / self.debt_value(cup_id) * Decimal(100)
-        return round(coll_ratio, 3)
-
-    def system_collateralization_ratio(self):
-        totalWethLocked = Decimal(self.tub.pie())
-        wethPrice = Decimal(self.pip.eth_price()) / 10 ** 18
-        daiSupply = Decimal(self.dai.totalSupply())
-        targetPrice = Decimal(self.vox.par()) / self.RAY
-        totalCollateralValue = totalWethLocked / self.WAD * wethPrice
-        systemDaiDebt = daiSupply * targetPrice
-        scr = totalCollateralValue / systemDaiDebt
-        return scr * 10 ** 18 * 100
-
-   # const totalCollateralValue = new BigNumber(totalWethLocked)
-   #   .div(WAD)
-   #   .times(wethPrice.toBigNumber());
-   # const systemDaiDebt = daiSupply.times(targetPrice);
-   # return totalCollateralValue.div(systemDaiDebt.toBigNumber()).toNumber();
-
-    #self.peth_price() 
-
-#  self.tag()* self.target_price()
-
-    def peth_available_to_withdraw(self, cup_id):
-        tag = Decimal(self.tub.tag())
-        return self.collateral_peth_value(cup_id) / self.WAD - Decimal(150) / (tag/self.RAY) /  Decimal(100) * self.debt_value(cup_id) / self.WAD 
-
-    def eth_available_to_withdraw(self, cup_id):
-        return self.peth_price() * self.peth_available_to_withdraw(cup_id)
-
-    def dai_available_to_generate(self, cup_id):
-        tag = Decimal(self.tub.tag())
-        debt_to_reach_150 =  ( Decimal(1) / (Decimal(1.50) / ( self.collateral_peth_value(cup_id) * (tag / self.RAY) )  ) ) / self.WAD
-        return debt_to_reach_150 - (self.debt_value(cup_id) / self.WAD)
-
-    def stability_fee(self):
-        fee = self.tub.fee()
-        seconds_per_year = 60 * 60 * 24 * 365
-        compounded_fee = (pow(fee / self.RAY, seconds_per_year) - 1) * 100
-        return round(Decimal(compounded_fee), 2)
-
-    def report(self):
-        cup_id = 15341
-        print("liquidation price: $", round(self.liquidation_price(cup_id), 3))
-        print("ether price: $", self.pip.eth_price() / self.WAD)
-        print("liquidation penalty: ", self.liquidation_penalty(), "%")
-        print("collateralization ratio: ", self.collateralization_ratio(cup_id), "%")
-        print("collateral peth value: PETH", round(self.node.w3.fromWei(self.collateral_peth_value(cup_id) , 'ether'), 3))
-        print("collateral eth value: ETH", round(self.node.w3.fromWei(self.collateral_eth_value(cup_id), 'ether'), 3))
-        print("yearly stability fee: ", self.stability_fee(), "%")
-        print("CDP eth value: $", round( Decimal(self.pip.eth_price() / self.WAD) * self.node.w3.fromWei(self.collateral_eth_value(cup_id), 'ether'), 3) )
-        print("global dai available: DAI ", round(self.global_dai_available() / self.WAD) )
-        print("max available to withdraw PETH ", round(self.peth_available_to_withdraw(cup_id), 3) )
-        print("max available to withdraw ETH ", round(self.eth_available_to_withdraw(cup_id), 3) )
-        print("max available to withdraw value $", round(self.eth_available_to_withdraw(cup_id) * Decimal(self.pip.eth_price()) / self.WAD, 2))
-        print("dai generated by cdp: DAI ", round(self.tub.tab(cup_id) / self.WAD, 2))
-        print("max dai available to generate: DAI ", round(self.dai_available_to_generate(cup_id), 3))
-        print("global CDP collateralization ratio: ", round(self.system_collateralization_ratio(), 4) )
-        print("MKR price:  USD ", round(self.pep.mkr_price() / self.WAD, 3))
-        debug(); pdb.set_trace()
-
+    #debug(); pdb.set_trace()
 
     def initialize(self):
-        #self.name = None  # The full name given by the user
-        #self.subdomain = None  # just the subdomain
-        #self.domain = None # just the domain.eth
-        #self.resolved_address = None 
-
         if self.node.network_name not in ["MainNet", "Kovan"]:
             self.add_message_dialog("This Dapp only functions on the Ethereum MainNet and Kovan")
             return
@@ -157,13 +83,14 @@ class Dapp(SLDapp):
         self.pip = SaiPip(self.node)
         self.pep = SaiPep(self.node)
 
-        debug(); pdb.set_trace()
-        self.report()
 
+        self.add_frame(CDPStatusFrame, height=25, width=55, title="CDP {} info".format(self.cup_id))
+
+    def getCdpIds():
         query = "query ($lad: String) {\n      allCups(condition: { lad: $lad }) {\n        nodes {\n          id\n        }\n      }\n    }"
         variables = '{"lad":"0x0E3873CC74F363aA38C2D8F1a29F6D1480078D55"}'
 
-        #q = '{"query":"query ($lad: String) {\n      allCups(condition: { lad: $lad }) {\n        nodes {\n          id\n        }\n      }\n    }","variables":{"lad": "0x0E3873CC74F363aA38C2D8F1a29F6D1480078D55"}}'
+        # q = '{"query":"query ($lad: String) {\n      allCups(condition: { lad: $lad }) {\n        nodes {\n          id\n        }\n      }\n    }","variables":{"lad": "0x0E3873CC74F363aA38C2D8F1a29F6D1480078D55"}}'
         gql_string = '''{
   allCups(
     condition: {lad: "0x0E3873CC74F363aA38C2D8F1a29F6D1480078D55"}
@@ -175,23 +102,6 @@ class Dapp(SLDapp):
   }  
 }'''
         gql_string2 = '{ allCups(){ totalCount } }'
-
-
-# async getCollateralizationRatio(cdpId) {
-#    const usdDebt = await this.getDebtValue(cdpId, USD);
-#    // avoid division by 0
-#    if (usdDebt.eq(0)) return Infinity;
-#
-#    const [pethPrice, pethCollateral] = await Promise.all([
-#      this.get('price').getPethPrice(),
-#      this.getCollateralValue(cdpId, PETH)
-#    ]);
-#    return pethCollateral
-#      .times(pethPrice)
-#      .div(usdDebt)
-#      .toNumber();
-#  }
-
 
 
         #0x0E3873CC74F363aA38C2D8F1a29F6D1480078D55
@@ -238,33 +148,99 @@ class Dapp(SLDapp):
 
         #self.price_poller.eth_price
 
-        self.add_frame(CDPStatusFrame, height=20, width=55, title="CDP info")
-  # this.getDebtValue(cdpId, USD),
-        #debt_value = 
 
-     # this.getLiquidationRatio(),
-     # this.getCollateralValue(cdpId)
 
-     #   liquidation_price = debt_value * liquidation_ratio / collateral_value
 
-'''  
-async getLiquidationPenalty() {
-    const value = await this._tubContract().axe();
+    def peth_price(self):
+        per = Decimal(self.tub.per())
+        return per / self.RAY
+ 
+    def collateral_eth_value(self, cup_id):
+        return self.peth_price() * self.collateral_peth_value(cup_id)
 
-    return new BigNumber(value.toString())
-      .dividedBy(RAY)
-      .minus(1)
-      .toNumber();
-  }
+    def liquidation_ratio(self):
+        mat = Decimal(self.tub.mat())
+        return  mat / self.RAY 
+        
+    def liquidation_price(self, cup_id):
+        debt_value = Decimal(self.tub.tab(cup_id)) # not quite, but..
+        return debt_value * self.liquidation_ratio() / self.collateral_eth_value(cup_id)
 
-'''
-      
+    def collateral_peth_value(self, cup_id):
+        return Decimal(self.tub.ink(cup_id))
 
-        #try:
-            #    pass
-            #except (ContractConfigError):
-                #    self.add_message_dialog("Could not load the contracts for this Dapp.")
-                #    return None
+    # abstract collateral price
+    def tag(self):
+        return Decimal(self.tub.tag()) / self.RAY
+
+    def liquidation_penalty(self):
+        axe = self.tub.axe()
+        penalty = ((axe / self.RAY) - 1) * 100
+        return round(Decimal(penalty), 2)
+
+    def debt_value(self, cup_id):
+        return Decimal(self.tub.tab(cup_id))
+
+    def global_dai_available(self):
+        return Decimal(self.tub.rum()) 
+
+    def target_price(self):
+        return Decimal(self.vox.par())
+
+    def collateralization_ratio(self, cup_id):
+        tag = Decimal(self.tub.tag())
+        coll_ratio = self.collateral_peth_value(cup_id) * (tag / self.RAY) / self.debt_value(cup_id) * Decimal(100)
+        return round(coll_ratio, 3)
+
+    def system_collateralization_ratio(self):
+        totalWethLocked = Decimal(self.tub.pie())
+        wethPrice = Decimal(self.pip.eth_price()) / 10 ** 18
+        daiSupply = Decimal(self.dai.totalSupply())
+        targetPrice = Decimal(self.vox.par()) / self.RAY
+        totalCollateralValue = totalWethLocked / self.WAD * wethPrice
+        systemDaiDebt = daiSupply * targetPrice
+        scr = totalCollateralValue / systemDaiDebt
+        return scr * 10 ** 18 * 100
+
+    def peth_available_to_withdraw(self, cup_id):
+        tag = Decimal(self.tub.tag())
+        return self.collateral_peth_value(cup_id) / self.WAD - Decimal(150) / (tag/self.RAY) /  Decimal(100) * self.debt_value(cup_id) / self.WAD 
+
+    def eth_available_to_withdraw(self, cup_id):
+        return self.peth_price() * self.peth_available_to_withdraw(cup_id)
+
+    def dai_available_to_generate(self, cup_id):
+        tag = Decimal(self.tub.tag())
+        debt_to_reach_150 =  ( Decimal(1) / (Decimal(1.50) / ( self.collateral_peth_value(cup_id) * (tag / self.RAY) )  ) ) / self.WAD
+        return debt_to_reach_150 - (self.debt_value(cup_id) / self.WAD)
+
+    def stability_fee(self):
+        fee = self.tub.fee()
+        seconds_per_year = 60 * 60 * 24 * 365
+        compounded_fee = (pow(fee / self.RAY, seconds_per_year) - 1) * 100
+        return round(Decimal(compounded_fee), 2)
+
+    def ether_price(self):
+        return self.pip.eth_price() / self.WAD
+
+    def report(self):
+        print("liquidation price: $", round(self.liquidation_price(cup_id), 3))
+        print("ether price: $", self.ether_price())
+        print("liquidation penalty: ", self.liquidation_penalty(), "%")
+        print("collateralization ratio: ", self.collateralization_ratio(cup_id), "%")
+        print("collateral peth value: PETH", round(self.node.w3.fromWei(self.collateral_peth_value(cup_id) , 'ether'), 3))
+        print("collateral eth value: ETH", round(self.node.w3.fromWei(self.collateral_eth_value(cup_id), 'ether'), 3))
+        print("yearly stability fee: ", self.stability_fee(), "%")
+        print("CDP eth value: $", round( Decimal(self.pip.eth_price() / self.WAD) * self.node.w3.fromWei(self.collateral_eth_value(cup_id), 'ether'), 3) )
+        print("global dai available: DAI ", round(self.global_dai_available() / self.WAD) )
+        print("max available to withdraw PETH ", round(self.peth_available_to_withdraw(cup_id), 3) )
+        print("max available to withdraw ETH ", round(self.eth_available_to_withdraw(cup_id), 3) )
+        print("max available to withdraw value $", round(self.eth_available_to_withdraw(cup_id) * Decimal(self.pip.eth_price()) / self.WAD, 2))
+        print("dai generated by cdp: DAI ", round(self.tub.tab(cup_id) / self.WAD, 2))
+        print("max dai available to generate: DAI ", round(self.dai_available_to_generate(cup_id), 3))
+        print("global CDP collateralization ratio: ", round(self.system_collateralization_ratio(), 4) )
+        print("MKR price:  USD ", round(self.pep.mkr_price() / self.WAD, 3))
+        debug(); pdb.set_trace()
 
 
 
