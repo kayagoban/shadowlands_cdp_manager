f83b3030094d8184cfbf0fa4b30ea1c8f4929f9b
C Thomas <cthomas@soykaf.digital>
2019-05-31 09:07:46 -0500

    Better input validation and display for free and lock

--- lock_eth_frame.py
+++ lock_eth_frame.py
@@ -18,7 +18,7 @@ class LockEthFrame(SLFrame):
         self.add_label("How much ETH would you like to deposit?", add_divider=False)
         self.deposit_textbox_value = self.add_textbox("ETH Value:", default_value='1')
         self.add_label("Current account balance (ETH):", add_divider=False)
-        self.add_label(str(self.dapp.node.eth_balance)[0:12])
+        self.add_label("{:f}".format(self.dapp.node.eth_balance)[0:12])
         self.add_label("Projected liquidation price:", add_divider=False)
         self.add_label(self.projected_liquidation_price)
         self.add_label("Projected collateralization ratio:", add_divider=False)
@@ -31,9 +31,10 @@ class LockEthFrame(SLFrame):
         )
 
     def lock_eth_choice(self):
-        if self.deposit_eth_value() == Decimal(0.0):
-            self.dapp.add_message_dialog("0 is not a valid choice")
+        if self.deposit_eth_value() < Decimal(0):
+            self.dapp.add_message_dialog("Not a valid deposit amount")
             return
+            
 
         #debug(); pdb.set_trace()
 
@@ -53,29 +54,23 @@ class LockEthFrame(SLFrame):
         return
  
 
-        #self.dapp.add_transaction_dialog(
-        #    self.dapp.ds_proxy.lock(
-        #        self.dapp.tub.address, 
-        #        self.dapp.cup_id, 
-        #        self.deposit_eth_value()
-        #    ),
-        #    tx_value=self.deposit_eth_value(),
-        #    title="Lock Collateral",
-        #    gas_limit=230000,
-        #)
- 
-        #self.dapp.lock_eth(self.dapp.cup_id, self.deposit_eth_value())
         self.close()
 
     def projected_liquidation_price(self):
         try:
-            return str(self.dapp.projected_liquidation_price(0, self.deposit_eth_value()))[0:12]
+            liq_price = self.dapp.projected_liquidation_price(0, self.deposit_eth_value())
+            if liq_price < Decimal(0):
+                return "Undefined"
+            return "{:f}".format(liq_price)[0:12] + " USD"
         except (decimal.InvalidOperation):
             return "Undefined"
 
     def projected_collateralization_ratio(self):
         try:
-            return str(self.dapp.projected_collateralization_ratio(0,  self.deposit_eth_value()))[0:12]
+            proj_ratio = self.dapp.projected_collateralization_ratio(0,  self.deposit_eth_value())
+            if proj_ratio < Decimal(0):
+                return "Undefined"
+            return "{:f}".format(proj_ratio)[0:12] + " %"
         except (decimal.DivisionByZero, decimal.InvalidOperation):
             return "Undefined"
 
